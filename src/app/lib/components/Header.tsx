da2e5861f37f265b23818fa3d8a28c56009a6805
Harukotokoto <n_rui@outlook.jp>
2024-05-28 18:47:39 +0900

    ハンバーガーメニューの基盤を作成

--- src/app/lib/components/Header.tsx
+++ src/app/lib/components/Header.tsx
@@ -8,19 +8,54 @@ import {
   InputGroup,
   InputLeftElement,
 } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 import { BsCart } from 'react-icons/bs'
 
 const Header = () => {
+  const [menuState, setMenuState] = useState(false)
+  const [cartMenuState, setCartMenuState] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuState(!menuState)
+  }
+
+  const toggleCartMenu = () => {
+    setCartMenuState(!cartMenuState)
+  }
+
   return (
     <Box height={'64px'} px={'40px'} display={'flex'}>
-      {/* メニューのボタン */}
+      <Box>
+        <Box
+          backgroundColor={'#ffffff'}
+          width={'252px'}
+          height={'100vh'}
+          position={'fixed'}
+          display={menuState ? 'flex' : 'none'}
+          zIndex={10000}
+          left={0}
+          top={0}
+        ></Box>
+        <Box
+          top={0}
+          left={0}
+          backgroundColor={'rgba(0,0,0,0.4)'}
+          width={'100vw'}
+          height={'100vh'}
+          onClick={() => toggleMenu()}
+          position={'fixed'}
+          zIndex={9999}
+          display={menuState ? 'flex' : 'none'}
+        ></Box>
+      </Box>
+      {/*メニューのボタン*/}
       <IconButton
         my={'auto'}
         mx={'10px'}
         backgroundColor={'rgba(0,0,0,0)'}
         aria-label="メニュー"
         icon={<Icon as={HamburgerIcon} boxSize={6} />}
+        onClick={toggleMenu}
       />
 
       {/*ヘッダーの文字*/}
@@ -42,6 +77,7 @@ const Header = () => {
         my={'auto'}
         backgroundColor={'rgba(0,0,0,0)'}
         icon={<Icon as={BsCart} boxSize={6} />}
+        onClick={toggleCartMenu}
       />
     </Box>
   )
