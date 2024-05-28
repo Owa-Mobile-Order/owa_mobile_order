7829b87233c6a65d5d08f54f511db0f4ff6f0243
Harukotokoto <n_rui@outlook.jp>
2024-05-28 19:10:21 +0900

    サイドメニューを実装

--- src/app/lib/components/Header.tsx
+++ src/app/lib/components/Header.tsx
@@ -1,83 +1,24 @@
 'use client'
-import { HamburgerIcon, Icon, SearchIcon } from '@chakra-ui/icons'
-import {
-  Box,
-  Heading,
-  IconButton,
-  Input,
-  InputGroup,
-  InputLeftElement,
-} from '@chakra-ui/react'
-import React, { useState } from 'react'
+import { Icon } from '@chakra-ui/icons'
+import { Box, Heading, IconButton, Spacer } from '@chakra-ui/react'
 import { BsCart } from 'react-icons/bs'
+import { LeftMenu } from '@/app/lib/components/LeftMenu'
 
 const Header = () => {
-  const [menuState, setMenuState] = useState(false)
-  const [cartMenuState, setCartMenuState] = useState(false)
-
-  const toggleMenu = () => {
-    setMenuState(!menuState)
-  }
-
-  const toggleCartMenu = () => {
-    setCartMenuState(!cartMenuState)
-  }
-
   return (
-    <Box height={'64px'} px={'40px'} display={'flex'}>
-      <Box>
-        <Box
-          backgroundColor={'#ffffff'}
-          width={'252px'}
-          height={'100vh'}
-          position={'fixed'}
-          display={menuState ? 'flex' : 'none'}
-          zIndex={10000}
-          left={0}
-          top={0}
-        ></Box>
-        <Box
-          top={0}
-          left={0}
-          backgroundColor={'rgba(0,0,0,0.4)'}
-          width={'100vw'}
-          height={'100vh'}
-          onClick={() => toggleMenu()}
-          position={'fixed'}
-          zIndex={9999}
-          display={menuState ? 'flex' : 'none'}
-        ></Box>
-      </Box>
-      {/*メニューのボタン*/}
-      <IconButton
-        my={'auto'}
-        mx={'10px'}
-        backgroundColor={'rgba(0,0,0,0)'}
-        aria-label="メニュー"
-        icon={<Icon as={HamburgerIcon} boxSize={6} />}
-        onClick={toggleMenu}
-      />
-
+    <Box height={'64px'} px={'20px'} display={'flex'}>
+      <LeftMenu />
       {/*ヘッダーの文字*/}
       <Heading my={'auto'} fontSize={'2xl'}>
         Owa Mobile Order
       </Heading>
-
-      {/*検索ボックス*/}
-      <InputGroup width={'500px'} ml={'60px'} mr={'auto'} my={'auto'}>
-        <InputLeftElement pointerEvents="none">
-          <SearchIcon color="gray.300" />
-        </InputLeftElement>
-        <Input placeholder="ラーメン" />
-      </InputGroup>
-
+      <Spacer />
       {/*カートボタン*/}
       <IconButton
         aria-label={'カート'}
         my={'auto'}
         backgroundColor={'rgba(0,0,0,0)'}
         icon={<Icon as={BsCart} boxSize={6} />}
-        onClick={toggleCartMenu}
       />
     </Box>
   )
---
+++ src/app/lib/components/LeftMenu.tsx
@@ -0,0 +1,53 @@
+import { Box, IconButton } from '@chakra-ui/react'
+import { HamburgerIcon, Icon } from '@chakra-ui/icons'
+import React, { useState } from 'react'
+
+const LeftMenu = () => {
+  const [isOpen, setOpen] = useState(false)
+
+  const toggleOpen = () => {
+    setOpen(!isOpen)
+  }
+
+  return (
+    <>
+      <Box
+        backgroundColor={'#ffffff'}
+        width={'252px'}
+        height={'100vh'}
+        opacity={isOpen ? 1 : 0}
+        position={'fixed'}
+        zIndex={10000}
+        visibility={isOpen ? 'visible' : 'hidden'}
+        left={isOpen ? 0 : '-300px'}
+        top={0}
+        transition={'0.4s'}
+      ></Box>
+      <Box
+        top={0}
+        left={0}
+        backgroundColor={'#000000'}
+        opacity={isOpen ? 0.5 : 0}
+        width={'100vw'}
+        height={'100vh'}
+        onClick={() => toggleOpen()}
+        position={'fixed'}
+        visibility={isOpen ? 'visible' : 'hidden'}
+        zIndex={9999}
+        transition={'0.4s'}
+      ></Box>
+
+      {/*メニューのボタン*/}
+      <IconButton
+        my={'auto'}
+        mx={'10px'}
+        backgroundColor={'rgba(0,0,0,0)'}
+        aria-label="メニュー"
+        icon={<Icon as={HamburgerIcon} boxSize={6} />}
+        onClick={toggleOpen}
+      />
+    </>
+  )
+}
+
+export { LeftMenu }
