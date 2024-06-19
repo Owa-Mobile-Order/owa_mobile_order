ab5a58fc83bad7cde0a0677426ef3be7dd0334c2
Harukotokoto <y.games@outlook.jp>
2024-06-19 16:54:39 +0900

    サイドメニューをFWを使用した記述方法に書き換え

--- src/app/lib/components/LeftMenu.tsx
+++ src/app/lib/components/LeftMenu.tsx
@@ -1,58 +1,53 @@
-import { Box, IconButton } from '@chakra-ui/react';
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  IconButton,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { HamburgerIcon, Icon } from '@chakra-ui/icons';
-import React, { useState } from 'react';
+import React from 'react';
 import { useSession } from 'next-auth/react';
 import { LoginMenu } from './left_menu/LoginMenu';
 import { LogoutMenu } from './left_menu/LogoutMenu';
 
 const LeftMenu = () => {
-  const { data: _session, status } = useSession();
-  const [isOpen, setOpen] = useState(true);
+  // メニューの状態
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef(null);
 
-  const toggleOpen = () => {
-    setOpen(!isOpen);
-  };
+  // ユーザー情報を取得
+  const { data: _session, status } = useSession();
 
   return (
     <>
-      <Box
-        backgroundColor={'#ffffff'}
-        width={'300px'}
-        p={'24px'}
-        height={'100vh'}
-        opacity={isOpen ? 1 : 0}
-        position={'fixed'}
-        zIndex={10000}
-        visibility={isOpen ? 'visible' : 'hidden'}
-        left={isOpen ? 0 : '-300px'}
-        top={0}
-        transition={'0.4s'}
-      >
-        {status === 'authenticated' ? <LoginMenu /> : <LogoutMenu />}
-      </Box>
-      <Box
-        top={0}
-        left={0}
-        backgroundColor={'#000000'}
-        opacity={isOpen ? 0.5 : 0}
-        width={'100vw'}
-        height={'100vh'}
-        onClick={() => toggleOpen()}
-        position={'fixed'}
-        visibility={isOpen ? 'visible' : 'hidden'}
-        zIndex={9999}
-        transition={'0.4s'}
-      ></Box>
-
-      {/*メニューのボタン*/}
       <IconButton
         my={'auto'}
         mx={'10px'}
         backgroundColor={'rgba(0,0,0,0)'}
         aria-label="メニュー"
         icon={<Icon as={HamburgerIcon} boxSize={6} />}
-        onClick={toggleOpen}
+        onClick={onOpen}
       />
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        size={'sm'}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader>
+            {status === 'authenticated' ? <LoginMenu /> : <LogoutMenu />}
+          </DrawerHeader>
+
+          <DrawerBody></DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
